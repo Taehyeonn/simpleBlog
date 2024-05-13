2956440c1b6f3e620ded8154efecc982a6cea78e
Taehyeonn <97taehyeon@gmail.com>
2024-05-13 16:25:52 +0900

    게시글 조회 기능 view 페이지 구현

--- src/main/java/com/example/simpleBlog/board/presentation/BoardController.java
+++ src/main/java/com/example/simpleBlog/board/presentation/BoardController.java
@@ -3,13 +3,13 @@ package com.example.simpleBlog.board.presentation;
 import com.example.simpleBlog.board.application.BoardService;
 import com.example.simpleBlog.board.dto.BoardInfo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
@@ -17,9 +17,11 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/{board_id}")
-    public ResponseEntity<BoardInfo> findBoardById(@PathVariable("board_id") final Long boardId) {
+    public String findBoardById(@PathVariable("board_id") final Long boardId,
+                                Model model) {
+
         BoardInfo response = boardService.findBoardById(boardId);
-        return ResponseEntity.ok()
-                .body(response);
+        model.addAttribute("boardInfo", response);
+        return "view";
     }
 }
